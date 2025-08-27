d093fd397fa3bdd2b2f1ffe4c5c721f262daf508
jwetherell86 <jsanfordwetherell@mail.valenciacollege.edu>
2025-08-27 10:50:48 -0400

    Add files via upload

---
+++ Python Variables.py
@@ -0,0 +1,43 @@
+#Multiplication
+number1 = int(input("Enter your first number to Multiply:\n"))
+number2 = int(input("Enter your second number to Multiply:\n"))
+answer1 = number1 * number2
+print(f"{number1} multiplied by {number2}")
+print("The answer to your query is:", answer1)
+
+
+# Division
+number3 = int(input("Enter your first number to Divide:\n"))
+number4 = int(input("Enter your second number to Divide:\n"))
+answer2 = number3 / number4
+print(f"{number3} divided by {number4}")
+print("The answer to your query is:", answer2)
+
+
+#  Division without remainder
+answer3 = number3 // number4
+print(f"{number3} divided by {number4} without remainder")
+print("The answer to your query is:", answer3)
+
+
+# Modulo
+answer4 = number3 % number4
+print(f"{number3} modulo {number4}")
+print("The answer to your query is:", answer4)
+
+# Add
+number6 = int(input("Enter your first number to Add:\n"))
+number7 = int(input("Ener your second number to Add:\n"))
+answer5 = number6 + number7
+print(f"{number6} add {number7}")
+print("The answer to your query is:", answer5)
+
+# Subtraction
+number8 = int(input("Enter your first number to Subtract:\n"))
+number9 = int(input("Ener your second number to Subtract:\n"))
+answer6 = number8 - number9
+print(f"{number8} minus {number9}")
+print("The answer to your query is:", answer6)
+
+
+print("Jack Sanford-Wetherell")
